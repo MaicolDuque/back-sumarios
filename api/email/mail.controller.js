d4f1f48f8b608c65150a9c3bc44c6e0a8bce4f92
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2020-11-18 19:08:02 -0500

    Add funcionality to save emails shiments and endpoint to retrieve by user ID

--- api/email/mail.controller.js
+++ api/email/mail.controller.js
@@ -2,7 +2,8 @@ const nodemailer = require('nodemailer'); // email sender function
 const { emailTransfer } = require('../../config');
 const ContactList = require("../contact-list/contact-list.model");
 const Summary = require("../summary/summary.model");
-const htmlSummaries = require('./info-email-summary')
+const htmlSummaries = require('./info-email-summary');
+const SendingHistory = require('./sending-history.model');
 
 
 function configTransporter() {
@@ -42,7 +43,7 @@ function sendEmail(req, res) {
 
 async function sendEmailSummary(req, res) {
 	try {
-		const { listaId, summaryId, name, name_magazine } = req.body
+		const { listaId, summaryId, name, name_magazine, userId } = req.body
 		const contacts = await ContactList.findOne({ _id: listaId }, { mg_contacts: 1 }).populate({ path: 'mg_contacts', model: 'Contact' }).exec()
 		const articles = await Summary.findOne({ _id: summaryId }, { list_articles: 1 }).populate({ path: 'list_articles', model: 'Article' }).exec()
 		const emailContacts = contacts.mg_contacts.map(contact => contact.c_email).toString()
@@ -52,13 +53,28 @@ async function sendEmailSummary(req, res) {
 			subject: name,
 			html: htmlSummaries(articles.list_articles, name_magazine)
 		};
+		await createSendingHistory(summaryId, userId, listaId)
 		return emailSend(mailOptions)
-		.then(info => res.send(info))
-		.catch(error => { res.status(500).send(error); console.log(error) })
+			.then(info => res.send(info))
+			.catch(error => { res.status(500).send(error); console.log(error) })
 	} catch (error) {
 		console.log(error)
 		return res.status(500).send(error)
 	}
+}
+
+function createSendingHistory(summary, user, contact_list) {
+	const send = new SendingHistory({ summary, user, contact_list })
+	return send.save()
+}
+
+function retrieveEmailSentsByUser(req, res) {
+	const { _id } = req.params
+	return SendingHistory.find({ user: _id })
+		.populate({ select: { name: 1, description: 1 }, path: 'summary', model: 'Summary' })
+		.populate({ select: { name: 1, description: 1 }, path: 'contact_list', model: 'ContactList' }).exec()
+		.then(info => res.send(info))
+		.catch(error => { res.status(500).send(error); console.log(error) })
 }
 
 async function emailSend(config) {
@@ -76,5 +92,6 @@ async function emailSend(config) {
 
 module.exports = {
 	sendEmail,
-	sendEmailSummary
+	sendEmailSummary,
+	retrieveEmailSentsByUser
 }
