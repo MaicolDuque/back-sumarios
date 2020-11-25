6a4d5bf8e6eabbff3a3740dd4fc7cfd6ec37bdf7
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2020-11-24 23:07:41 -0500

    Add config CORS

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ const cookieSession = require('cookie-session')
 const configDB = require('./db');
 const routesConfig = require('./routes');
 
-app.use(cors({
-    origin: config.url_front, // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true // allow session cookie from browser to pass through
-}));
+const whitelist = ['https://sumarios-poli.netlify.app/', 'https://sumarios-elpoli.vercel.app/', config.url_front]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true // allow session cookie from browser to pass through
+}
+
+// {
+//   origin: config.url_front, // allow to server to accept request from different origin
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true // allow session cookie from browser to pass through
+// }
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 
